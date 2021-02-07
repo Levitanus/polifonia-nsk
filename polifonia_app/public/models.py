8270ec4571d55f4519802889c88c5a11e858c854
levitanus <pianoist@ya.ru>
2021-02-08 03:41:17 +0700

    translit

--- polifonia_app/public/models.py
+++ polifonia_app/public/models.py
@@ -2,6 +2,8 @@ import typing as ty
 
 from pathlib import Path
 
+from transliterate import translit
+
 
 class Product:
     def __init__(self,
@@ -17,27 +19,28 @@ class Product:
         self.price = price
         self.lifetime = lifetime
         self.comment = comment
+        self.id_: str = translit(f'{type_} {name}', 'ru', True)
 
 
+ab4 = Product('Абонемент',
+              Path('abonement4.png'),
+              'на 4 занятия',
+              "3 000",
+              lifetime=("1 месяц", 1))
+ab8 = Product('Абонемент',
+              Path('abonement8.png'),
+              'на 8 занятий',
+              "5 200",
+              lifetime=("1 месяц", 1))
+ab24 = Product('Абонемент',
+               Path('abonement24.png'),
+               'на 24 занятия',
+               "13 200",
+               lifetime=("3 месяца", 1),
+               comment='Возможна рассрочка по абонементу в'
+               ' течение 2х месяцев: по 6 600₽')
 products = {
-    "Абонемент на 4 занятия":
-    Product('Абонемент',
-            Path('abonement4.png'),
-            'на 4 занятия',
-            "3 000",
-            lifetime=("1 месяц", 1)),
-    "Абонемент на 8 занятий":
-    Product('Абонемент',
-            Path('abonement8.png'),
-            'на 8 занятий',
-            "5 200",
-            lifetime=("1 месяц", 1)),
-    "Абонемент на 24 занятия":
-    Product('Абонемент',
-            Path('abonement24.png'),
-            'на 24 занятия',
-            "13 200",
-            lifetime=("3 месяца", 1),
-            comment='Возможна рассрочка по абонементу в'
-            ' течение 2х месяцев: по 6 600₽'),
+    ab4.id_: ab4,
+    ab8.id_: ab8,
+    ab24.id_: ab24,
 }
