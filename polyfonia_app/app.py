6a8e133880623bdd9a01184f55ae68813fe29dab
Timofey <pianoist@ya.ru>
2021-02-07 11:37:14 +0700

    test route

--- polyfonia_app/app.py
+++ polyfonia_app/app.py
@@ -37,5 +37,9 @@ def create_app(test_config: ty.Mapping[str, object] = None) -> Flask:
 
 application = create_app()
 
+@application.route("/")
+def hello():
+   return "<h1 style='color:blue'>Hello There!</h1>"
+
 if __name__ == "__main__":
    application.run(host='0.0.0.0')
